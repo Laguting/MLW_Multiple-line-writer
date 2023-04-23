48606b3e9e6fe672b6c2051b60609ef9ddf134b5
Laguting <lagutingmariconjaneg@gmail.com>
2023-04-23 13:39:12 +0800

    Placed the tentative psuedocodes

--- MLW.py
+++ MLW.py
@@ -1,3 +1,8 @@
 # Laguting, Maricon Jane G.
 # BSCPE 1-4 
 # Task: Write a method in python to write multiple line of text contents into a text file mylife.txt. 
+# Welcome the user
+# Open file and allow it to be rewritten
+# Your One line Diary
+# greet the user
+# Ask the user to enter any response
